9472e8133eba9f6ba14272fef8b084a84ff7e79d
oh-chaeyeon <chaebread555@gmail.com>
2024-06-13 01:41:43 +0900

    feat : 2-4 Link 컴포넌트, 2-6 useRouter 쿼리 사용하기

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,3 +1,8 @@
+import { useRouter } from "next/router";
+
 export default function Product() {
-  return <div>Product 페이지</div>;
+  const router = useRouter();
+  const { id } = router.query; // router.query라는 객체에서 id값을 가져옴. 여기서 id는 파일명 [id]이다.
+
+  return <div>Product {id} 페이지</div>;
 }
